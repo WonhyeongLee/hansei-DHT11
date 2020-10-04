1b89f57b1397c36357903542b376621b98e977c8
hansei-it <bigfile57@naver.com>
2020-10-05 03:26:23 +0900

    ejs 페이지 분리

--- example-express/routes/index.js
+++ example-express/routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var maria = require('../maria');
-var sql ='SELECT * FROM collect_data ORDER BY collect_time DESC LIMIT 10';
+var sql = 'SELECT * FROM collect_data ORDER BY collect_time DESC LIMIT 10';
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    data: 'Express'
+    data: 'main page'
   });
 });
 
@@ -15,38 +15,38 @@ router.get('/temp_c', function (req, res) {
       console.log('error: ', err.message);
       res.send(err);
     } else {
-      res.render('index',{
-          data:rows
-        });
-   }
- })
+      res.render('../views/part/temp_c.ejs', {
+        data: rows
+      });
+    }
+  })
 });
 
 router.get('/temp_f', function (req, res) {
-    maria.query(sql, function (err, rows) {
+  maria.query(sql, function (err, rows) {
     if (err) {
       console.log('error: ', err.message);
       res.send(err);
     } else {
-      res.render('index',{
-          data:rows
-        });
+      res.render('index', {
+        data: rows
+      });
     }
   })
 });
 
 router.get('/humid', function (req, res) {
-     maria.query(sql, function (err, rows) {
+  maria.query(sql, function (err, rows) {
     if (err) {
       console.log('error: ', err.message);
       res.send(err);
     } else {
-      res.render('index',{
-          data:rows
-        });
-   }
- })
-   
+      res.render('index', {
+        data: rows
+      });
+    }
+  })
+
 
 });
 
